250034ee80354cae5094199ae115e8b71e8c965f
jremmons <emmons.john@gmail.com>
2017-01-19 20:36:34 -0800

    added show.sh to setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 # usage: source setup.sh
 
 alias playback.sh="/home/john/Work/Captain-Eo/scripts/playback.sh"
-alias capture.sh="/home/john/Work/Captain-Eo/scripts/caputure.sh"
+alias capture.sh="/home/john/Work/Captain-Eo/scripts/capture.sh"
+alias show.sh="/home/john/Work/Captain-Eo/scripts/show.sh"
