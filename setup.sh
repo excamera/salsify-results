5dd0d1b46aeab1cb24fa55252a85e4c51d098c7f
jremmons <emmons.john@gmail.com>
2017-01-19 23:27:53 -0800

    added salsify log file; updated setup.sh; added more gitignores

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@
 alias playback.sh="/home/john/Work/Captain-Eo/scripts/playback.sh"
 alias capture.sh="/home/john/Work/Captain-Eo/scripts/capture.sh"
 alias show.sh="/home/john/Work/Captain-Eo/scripts/show.sh"
+alias run_analysis.py="python /home/john/Work/Captain-Eo/src/analysis/run_analysis.py"
