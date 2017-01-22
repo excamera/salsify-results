a4276115df918bf6f99f6bfea9dc28e0ddac7ac2
Emre Orbay <eorbay@stanford.edu>
2017-01-21 18:53:43 -0800

    begin getting results from real experiments

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# usage: source setup.sh
-
-alias playback.sh="/home/john/Work/Captain-Eo/scripts/playback.sh"
-alias capture.sh="/home/john/Work/Captain-Eo/scripts/capture.sh"
-alias show.sh="/home/john/Work/Captain-Eo/scripts/show.sh"
-alias run_analysis.py="python /home/john/Work/Captain-Eo/src/analysis/run_analysis.py"
